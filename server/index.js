d2861a5994c12b2a27572f96ab72f26c64391f9d
ericzaba <eric.zaba@gmail.com>
2022-03-13 07:13:55 -0500

    fuck shit cunt fuck fuck fuckasdfsdfsds fuck fuck fuck

--- server/index.js
+++ server/index.js
@@ -57,6 +57,14 @@ app.post("/upload_image", (req, res)=>{
 })
 
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")))
+      
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
+
 app.listen(()=>{
   console.log("server created and listening on " + PORT)
 })
