e1d871ff5fcb91236b517333532aef253e702de1
ericzaba <eric.zaba@gmail.com>
2022-03-13 01:43:07 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,69 @@
+require("dotenv").config()
+const express = require('express')
+const app = express()
+const PORT = 8080
+const AWS = require("aws-sdk")
+const axios = require("axios")
+const { S3 } = require("aws-sdk")
+
+const http = require("http")
+const https = require('https');
+const fs = require('fs');
+
+
+
+const awsBucketName = process.env.AWS_BUCKET_NAME
+const awsRegion = process.env.AWS_BUCKET_REGION
+const awsKey = process.env.AWS_BUCKET_KEY
+const awsSecret = process.env.AWS_SECRET_KEY 
+
+const s3 = new S3({
+  region: awsRegion, 
+  accessKeyId: awsKey, 
+  secretAccessKey: awsSecret
+})
+
+
+app.get('/test', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/get_next_item", (req, res) =>{
+    
+})
+
+app.get("/get_new_items", (req, res) => {
+  if (req.body.category === "none"){
+    var responseObj = {}
+  }
+})
+
+app.post("/enter_new_item", (req, res) => {
+  
+
+})
+
+app.post("/upload_image", (req, res)=>{
+  console.log("")
+  console.log("request from client", req.body)
+  s3.upload({
+    Bucket: awsBucketName,
+    Body: req.body.image,
+    Key: req.body.file.name
+  }).promise().then((result) => {
+    res.send(result)
+  })
+})
+
+
+const credentials = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8443);
+httpsServer.listen(PORT);
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import likeImage from "./thumbup.svg"
+import dislikeImage from "./thumbdown.svg"
+import axios from 'axios';
 function App() {
+
+  const [pageContent, setPageContent] = useState("UploadObjectView")
+
+  var content = {}
+
+  if (pageContent === "RankerWindow"){
+    content = <RankerWindow numberOfItems={1} category="none" />
+  }
+  if (pageContent === "UploadObjectView"){
+    content = <UploadObjectView />
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+        <h1>
+          Welcome to the best things in the universe.
+        </h1>
+        <h2>vote on a few items to get started!</h2>
+        <div style={{display: "flex", justifyContent: "center"}}>
+        {content}
+        </div>
     </div>
   );
+}
+
+
+interface RankerWindowProps{
+  numberOfItems: number
+  category: string
+}
+
+const RankerWindow = (props: RankerWindowProps) =>{
+  return(
+    <div style={{width: "288pt", height: "432pt", backgroundColor: "grey"}}>
+      <div style={{ display: "flex", width: "288pt", height: "72pt", justifyContent: "center", marginTop: "360pt"}}>
+        <img style={{backgroundImage: `url(${likeImage}`, 
+        backgroundRepeat: "no-repeat", backgroundSize: "cover", width: "48pt", height: "48pt"}} />
+        <img style={{backgroundImage: `url(${dislikeImage}`,
+         backgroundRepeat: "no-repeat", backgroundSize: "cover", width: "48pt", height: "48pt"}} />
+      </div>
+    </div>)
+}
+
+interface CardProps{
+  image: string
+  category: string
+  upvotes: number
+  downvotes: number
+}
+const RankerCard = (props: CardProps) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+function uploadObject(uploadItem: UploadItem){
+
+
+}
+
+
+
+interface UploadItem{
+  name: string
+  description: string
+  imageUrl: string
+  categories: string[]
+  upvotes: number
+  downvotes: number
+}
+
+
+const UploadObjectView = () => {
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+  const [categories, setCategories] = useState("")
+  const [imageUrl, setImageUrl] = useState("")
+
+  var uploadItem: UploadItem = {name: name, description: description, imageUrl: imageUrl, categories: [categories], upvotes: 0, downvotes: 0}
+
+  return(
+    <div>
+      <h2>Name</h2>
+      <input type="text"
+        value={name}
+        onChange={(e) => {setName(e.target.value)}} ></input>
+      <h2>Description</h2>
+      <input value={description}
+        onChange={(e) => {setDescription(e.target.value)}} ></input>
+      <h2>Picture</h2>
+      <input type="file" onChange={ (e) => { if ( e.target.files !== null ) {uploadImageToS3( e.target.files[0] )}}}></input>
+      <h2>Categories</h2>
+      <input value={categories}
+        onChange={(e) => {setCategories(e.target.value)}} ></input>
+
+      <button onClick={()=>{uploadObject(uploadItem)}}>Upload Object</button>
+    </div>
+  
+  )
+
+
+  function uploadImageToS3(image: File){
+    const uploadData:FormData = new FormData()
+    uploadData.append(image.name, image)
+    axios.post("https://localhost:8080/https://floating-harbor-33765.herokuapp.com/upload_image", uploadData)
+      .then((response)=>{
+        console.log("response from AWS", response)
+      })
+      .catch((error)=>{
+        console.log("failure from AWS", error)
+      })
+
+
+  }
+  
 }
 
 export default App;
