040dcbded5d916501863624356fa5cbad875abe5
ericzaba <eric.zaba@gmail.com>
2022-03-13 19:45:04 -0500

    the numbers mason

--- server/index.js
+++ server/index.js
@@ -42,7 +42,10 @@ app.post("/enter_new_item", (req, res) => {
 
 app.post("/upload_image", (req, res)=>{
   console.log("got /upload_image")
+
+  console.log("file test", req.files.file)
   const bb = busboyConstructor({headers: req.headers})
+  
   bb.on("file", (fieldname, file, filename, encoding, mimetype) => {
     console.log("got to on file busboy", file)
     s3.putObject({
